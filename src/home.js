1cda943818052b81e8216310d44a815175e5562a
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-09-09 18:53:48 +0400

    feat: complete createMain function in home.js module

--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
-export default function createHeader() {
-  const content = document.querySelector("#content");
+const content = document.querySelector("#content");
+
+
+function createHeader() {
   const header = document.createElement("header");
 
   const logo = document.createElement("div");
@@ -26,4 +28,70 @@ export default function createHeader() {
   header.appendChild(nav);
 
   content.appendChild(header);
+}
+
+
+function createMain() {
+  const main = document.createElement("main");
+
+  const left = document.createElement("div");
+  left.classList.add("left");
+
+  const text = document.createElement("div");
+  text.classList.add("text");
+  text.textContent = "Are you hungry?";
+  
+  const br1 = document.createElement("br");
+  text.appendChild(br1);
+
+  const span1 = document.createElement("span");
+  span1.setAttribute("id", "big-text");
+  span1.textContent = "Don't wait!";
+  text.appendChild(span1);
+  const br2 = document.createElement("br");
+  text.appendChild(br2);
+  
+  const span2 = document.createElement("span");
+  span2.setAttribute("id", "small-text");
+  span2.textContent = "Let start to order food now";
+  text.appendChild(span2);
+
+  left.appendChild(text);
+
+  const btnContainer = document.createElement("div");
+  btnContainer.classList.add("btn-container");
+  const button = document.createElement("button");
+  button.setAttribute("id", "check-menu");
+  button.textContent = "Check out menu";
+  btnContainer.appendChild(button);
+
+  left.appendChild(btnContainer);
+
+  main.appendChild(left);
+
+
+  const right = document.createElement("right");
+  right.classList.add("right");
+
+  const img = document.createElement("img");
+  img["src"] = "./images/dish.png";
+  img["alt"] = "dish";
+
+  right.appendChild(img);
+
+
+  content.appendChild(main);
+
+  main.appendChild(right);
+}
+
+
+export default function createHomePage() {
+  createHeader();
+  createMain();
+}
+
+export {
+  createHeader,
+  createMain
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import createHeader from "./home";
+import createHomePage from "./home";
 
-createHeader();
+createHomePage();
